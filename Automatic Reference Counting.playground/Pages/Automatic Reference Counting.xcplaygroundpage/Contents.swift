4c0b00c2678c0f38ea3a2241e21804ff6703b6d9
Matthew Harding <matthewpharding@gmail.com>
2022-10-16 11:08:34 +0100

    Added the page number (from official Swift documentation) to each Xcode playground file.

--- Automatic Reference Counting.playground/Pages/Automatic Reference Counting.xcplaygroundpage/Contents.swift
+++ Automatic Reference Counting.playground/Pages/Automatic Reference Counting.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 //: ![Swift](swift-logo.png)
 //: Swift v5.7 [official docs](https://docs.swift.org/swift-book/LanguageGuide/AutomaticReferenceCounting.html)
-//: # Automatic Reference Counting
+//: # 25. Automatic Reference Counting
 //:
 //:
 //: Swift uses Automatic Reference Counting (ARC) to track and manage your app’s memory usage. In most cases, this means that memory management “just works” in Swift, and you don’t need to think about memory management yourself. ARC automatically frees up the memory used by class instances when those instances are no longer needed.
